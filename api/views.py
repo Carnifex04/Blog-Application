205cd30cd059a6a4209492b3edebd030a1aece27
hriday <guptahriday4444@gmail.com>
2022-09-28 16:17:01 +0530

    added UserList and UserDetail views to the api app

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+import serializers
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+# UserList view provides read-only access to the list of users
+class UserList(generics.ListAPIView):
+    # list of model instances
+    queryset = User.objects.all()
+    # serializes the User data
+    serializer_class = serializers.UserSerializer
+
+
+# UserDetail view provides read-only access to a single user.
+class UserDetail(generics.RetrieveAPIView):
+    # list of model instances
+    queryset = User.objects.all()
+    # serializes the User data
+    serializer_class = serializers.UserSerializer
