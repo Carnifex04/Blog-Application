afb9d32ac95b522278a7f9e4a0fab3a73d1655bc
hriday <guptahriday4444@gmail.com>
2022-09-28 16:45:04 +0530

    added Post model and linked it to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import Post
 
 # Register your models here.
+
+# Registering the admin site
+admin.site.register(Post)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    posted = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True, default='')
+    title = models.CharField(max_length=150, blank=True, default='')
+    author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['posted']
