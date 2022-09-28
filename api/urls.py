06f589fce6427bc8e774573a0beaeae8de5cbbf3
hriday <guptahriday4444@gmail.com>
2022-09-28 16:22:27 +0530

    added url paths to the UserList and UserDetail views

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+import views
+
+urlpatterns = [
+    # corresponds to the UserList view
+    path('users/', views.UserList.as_view()),
+    # corresponds to the UserDetail view, using primary key(pk) as a keyword argument
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
